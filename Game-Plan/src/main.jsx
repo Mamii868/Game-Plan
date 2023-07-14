eac4169826730e444f6c7f0f198dc10f7c9e4d21
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-14 12:20:21 -0500

    removed placeholder navbar

--- Game-Plan/src/main.jsx
+++ Game-Plan/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "blue",
+    },
+    secondary: {
+      main: "green",
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "4rem",
+      fontWeight: 550,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
