14ec1af24365b13dfd498cb10a253ccacb8053ab
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-20 16:54:07 -0500

    stared basic server

--- Game-Plan/server/config/connection.js
+++ Game-Plan/server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/app-track',
+  process.env.MONGODB_URI || 'mongodb://localhost/gameplan',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
