5e1a8cedcbb8256fe4b7952283df3bbefcef2953
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-21 10:40:52 -0500

    installed apollo-server

--- Game-Plan/server/server.js
+++ Game-Plan/server/server.js
@@ -1,10 +1,19 @@
-const express = require('express')
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
+const {ApolloServer} = require("apollo-server")
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-    });
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
   
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
