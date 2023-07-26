e738df3fd9fb79e401639cae8fc66668b10d2e17
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-26 16:09:25 -0500

    added div

--- src/components/QuestList/QuestList.tsx
+++ src/components/QuestList/QuestList.tsx
@@ -1,9 +1,14 @@
 import { Typography } from "@mui/material";
+import supabase from "../../config/supabaseClient";
 const QuestList = () => {
+    console.log(supabase)
     return (
+        <div>
         <Typography variant="h1">
             Quest Board
         </Typography>
+
+        </div>
     )
 }
 
