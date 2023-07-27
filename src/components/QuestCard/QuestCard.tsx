84d151c24c8751a81f9ef53f6560a3e902b6f422
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-26 20:04:21 -0500

    Created basic list template for quests

---
+++ src/components/QuestCard/QuestCard.tsx
@@ -0,0 +1,18 @@
+import { CheckBox } from "@mui/icons-material";
+import { ListItem, ListItemButton,ListItemText } from "@mui/material";
+import { Quest } from "../../types/types";
+type QuestProp = {
+    quest: Quest;
+}
+const QuestCard: React.FC<QuestProp>= ({ quest }) => {
+  return (
+    <ListItem>
+        <ListItemButton>
+            <CheckBox></CheckBox>
+            <ListItemText primary={quest.questName} />
+        </ListItemButton>
+    </ListItem>
+  );
+};
+
+export default QuestCard
