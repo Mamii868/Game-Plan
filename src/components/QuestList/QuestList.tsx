1fd136d0365c1f152cb2297a501a801c4a81de9b
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-26 20:05:09 -0500

    fetches quests from database

--- src/components/QuestList/QuestList.tsx
+++ src/components/QuestList/QuestList.tsx
@@ -1,15 +1,42 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
 import supabase from "../../config/supabaseClient";
-const QuestList = () => {
-    console.log(supabase)
-    return (
-        <div>
-        <Typography variant="h1">
-            Quest Board
-        </Typography>
+import QuestCard from "../QuestCard/QuestCard";
+import { Quest } from "../../types/types";
 
-        </div>
-    )
-}
+const QuestList = () => {
+  const [fetchErr, setFetchErr] = useState<string | null>(null);
+  const [quests, setQuests] = useState<Quest[] |  null>(null);
+
+  useEffect(() => {
+    const fetchQuests = async () => {
+      const { data, error } = await supabase.from("quests").select()
+      if (error) {
+        setFetchErr('Cannot fetch quests')
+        setQuests(null)
+      }
+      if (data) {
+        setQuests(data)
+        setFetchErr(null)
+      }
+    };
+    fetchQuests()
+  });
+  return (
+    <div>
+      <Typography variant="h1">Quest Board</Typography>
+        {fetchErr && (<p>{fetchErr}</p>)}
+        {quests && (
+            <Box>
+            {quests.map(quest => (
+                <Typography variant="h6">
+                    <QuestCard quest={quest} key={quest.id}/>
+                </Typography>
+            ))}
+            </Box>
+        )}
+    </div>
+  );
+};
 
 export default QuestList;
