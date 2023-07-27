ed4c2353316fa67fde3ccd263e136ee34affb106
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-26 20:38:52 -0500

    added ability to load questDetails popup

--- src/components/QuestCard/QuestCard.tsx
+++ src/components/QuestCard/QuestCard.tsx
@@ -1,16 +1,28 @@
 import { CheckBox } from "@mui/icons-material";
 import { ListItem, ListItemButton, ListItemText } from "@mui/material";
+import {useState} from 'react'
 import { Quest } from "../../types/types";
-type QuestProp = {
+import QuestDetails from "../QuestDetails/QuestDetails";
+type QuestListProps = {
   quest: Quest;
 };
-const QuestCard: React.FC<QuestProp> = ({ quest }) => {
+const QuestCard: React.FC<QuestListProps> = ({ quest }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    }
+    
+    const handleClose = () => {
+        setOpen(false);
+    }
   return (
-    <ListItem>
+    <ListItem onClick={handleOpen}>
       <ListItemButton>
         <CheckBox></CheckBox>
         <ListItemText primary={quest.questName} />
       </ListItemButton>
+      <QuestDetails quest={quest} open={open} handleClose={handleClose}/>
     </ListItem>
   );
 };
