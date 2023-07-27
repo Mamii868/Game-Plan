2700ed998ddfe4469ff75baff185840fd8992cf8
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-07-26 20:03:40 -0500

    created types file

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export type Quest = {
+    id: number;
+    questName: string;
+    completed: boolean;
+    created_at: string; //may need to update to Date
+    questDesc: string | null;
+    categoryEXP: number | null;
+    rewardID: number | null;
+    deadline: string | null;
+    hidden: boolean;
+}
