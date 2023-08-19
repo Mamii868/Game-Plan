b35e25b8ef686d55a9f933d9b2da25c477a44c61
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-08-19 10:20:04 -0500

    new skill type

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,10 +2,19 @@ export type Quest = {
   id: number;
   questName: string;
   completed: boolean;
-  created_at: string; //may need to update to Date
+  created_at: Date;
   questDesc: string | null;
   categoryEXP: number | null;
   rewardID: number | null;
   deadline: string | null;
   hidden: boolean;
 };
+
+export type Skill = {
+  id: number;
+  skillName: string;
+  skillLevel: number;
+  skillEXP: number;
+  skillDesc: string | null;
+  skillColor: string;
+}
