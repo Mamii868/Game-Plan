460fe9af00d4f9f5a193485f384e04aa69d2d241
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-11-07 20:22:36 -0600

    added tokens function

--- src/theme.d.ts
+++ src/theme.d.ts
@@ -4,3 +4,21 @@ import * as React from "react";
 export function useMode(): [Theme, { toggleColorMode: () => void }];
 
 export const ColorModeContext: React.Context<{ toggleColorMode: () => void }>;
+
+export function tokens(mode: 'light' | 'dark'): {
+    greenAccent: {
+      [key: number]: string;
+    };
+    yellowAccent: {
+      [key: number]: string;
+    };
+    blackAccent: {
+      [key: number]: string;
+    };
+    grayAccent: {
+      [key: number]: string;
+    };
+    primary: {
+      [key: number]: string;
+    };
+  };
