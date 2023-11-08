bf46e4a2fc2c1b910fa63a511c5aa8a1d34e2275
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-11-07 20:12:03 -0600

    removed original theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Landing, Navbar, QuestList } from "./components";
-import {
-  CssBaseline,
-  ThemeProvider,
-  createTheme,
-  Theme,
-} from "@mui/material";
+import { CssBaseline, ThemeProvider} from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 const App = () => {
-  //dark mode theme
-  const theme: Theme = createTheme({
-    palette: {
-      mode: "dark",
-      background: {
-        default: "#393E41", // gray-black
-        paper: "#393E41", // teal
-      },
-      primary: {
-        main: "#44BBA4", // teal
-      },
-      secondary: {
-        main: "#FFD560", // yellow
-      },
-      text: {
-        primary: "#E7E5DF", // gray-white
-      },
-    },
-    typography: {
-      h1: {
-        fontSize: "4rem",
-        fontWeight: 550,
-      },
-    },
-  });
-
+  const [theme, colorMode] = useMode();
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-      <CssBaseline /> 
-      <Navbar />
-     <Routes>
-      <Route path="/" element={<Landing/>} />
-      <Route path="/quests" element={<QuestList/>}/>
-     </Routes>
-     </Router>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <CssBaseline />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/quests" element={<QuestList />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 };
 
