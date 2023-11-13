d26235270d147cda94d405879aed5c74a8925c60
MarcmotenDC <115433769+MarcmotenDC@users.noreply.github.com>
2023-11-13 10:11:50 -0600

    added signup export

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from "./QuestCard/QuestCard";
 export * from "./QuestDetails/QuestDetails";
 export * from "./QuestList/QuestList";
 export * from "./Landing/Landing";
+export * from "./SignUp/SignUp";
